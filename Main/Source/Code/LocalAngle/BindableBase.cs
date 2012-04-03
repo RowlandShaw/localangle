53b751bb4cf887eae20c4a7c3dd391dee84f58e7
rowlandshaw_cp <SND\rowlandshaw_cp>
2012-04-03 07:57:00 +0000

    Fix up INotifyPropertyChanging, to actually be called. oops.

--- Main/Source/Code/LocalAngle/BindableBase.cs
+++ Main/Source/Code/LocalAngle/BindableBase.cs
@@ -10,6 +10,8 @@ namespace LocalAngle
     [DataContract]
     public abstract class BindableBase : INotifyPropertyChanged, INotifyPropertyChanging
     {
+        #region Public Events
+
         /// <summary>
         /// Occurs when a property value changes.
         /// </summary>
@@ -20,13 +22,17 @@ namespace LocalAngle
         /// </summary>
         public event PropertyChangingEventHandler PropertyChanging;
 
+        #endregion
+
+        #region Protected Methods
+
         /// <summary>
         /// Checks if a property already matches a desired value.  Sets the property and
         /// notifies listeners only when necessary.
         /// </summary>
         /// <typeparam name="T">Type of the property.</typeparam>
         /// <param name="propertyName">Name of the property used to notify listeners.</param>
-        /// <param name="storage">Reference to a property with both getter and setter.</param>
+        /// <param name="storage">Reference to the backing field for a property with both getter and setter.</param>
         /// <param name="value">Desired value for the property.</param>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1045:DoNotPassTypesByReference", MessageId = "1#")]
         protected void OnPropertyChanged<T>(string propertyName, ref T storage, T value)
@@ -34,6 +40,7 @@ namespace LocalAngle
             if (!object.Equals(storage, value))
             {
                 //T oldValue = storage;
+                OnPropertyChanging(new PropertyChangingEventArgs(propertyName));
                 storage = value;
                 OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
             }
@@ -54,7 +61,7 @@ namespace LocalAngle
         /// <summary>
         /// Raises the <see cref="E:PropertyChanging"/> event.
         /// </summary>
-        /// <param name="e">The <see cref="System.ComponentModel.PropertyChangingEventArgs"/> instance containing the event data.</param>
+        /// <param name="e">The <see cref="System.ComponentModel.PropertyChangedEventArgs"/> instance containing the event data.</param>
         protected virtual void OnPropertyChanging(PropertyChangingEventArgs e)
         {
             if (PropertyChanging != null)
@@ -62,5 +69,7 @@ namespace LocalAngle
                 PropertyChanging(this, e);
             }
         }
+
+        #endregion
     }
 }
