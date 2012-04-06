197f7f09f09a9f81dda7faa012e5146426c2a9ff
rowlandshaw_cp <rowland.shaw@gmail.com>
2012-04-06 20:07:16 +0000

    Check in just enough of a mock of the LINQ to SQl bits to maintain source comaptibility of entities between Metro and other platforms

---
+++ Main/Source/Code/LocalAngle.Metro/System/ComponentModel/INotifyPropertyChanging.cs
@@ -0,0 +1,25 @@
+﻿namespace System.ComponentModel
+{
+    /// <summary>
+    /// Replicates just enough of LINQ to SQL to allow source compatibility for Metro for notifying clients that a property value is changing.
+    /// </summary>
+    public interface INotifyPropertyChanging
+    {
+        /// <summary>
+        /// Occurs when a property value is changing.
+        /// </summary>
+        event PropertyChangingEventHandler PropertyChanging;
+    }
+
+    /// <summary>
+    /// Replicates part of the framework not present for Metro to provide the data for the INotifyPropertyChanging.PropertyChanging event
+    /// </summary>
+    public class PropertyChangingEventArgs : PropertyChangedEventArgs
+    {
+        /// <summary>
+        /// The name of the property that is changing.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that is changing.</param>
+        public PropertyChangingEventArgs(string propertyName) : base(propertyName) { }
+    }
+}
