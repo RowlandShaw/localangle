1548e594eb4eba7082e72b49b3f7ff112701caa7
rowlandshaw_cp <rowland.shaw@gmail.com>
2013-04-20 14:05:44 +0000

    add solution, flesh out Credentials a touch (TFS Integration from 'tfsprevies' Id: 112;CreationDate: 2012-09-12 12:54:56 UTC;)

--- Main/Source/Code/LocalAngle/Diagnostics/DebugStreamWriter.cs
+++ Main/Source/Code/LocalAngle/Diagnostics/DebugStreamWriter.cs
@@ -11,6 +11,8 @@ namespace LocalAngle.Diagnostics
         /// <summary>
         /// Initializes a new instance of the <see cref="DebugTextWriter"/> class.
         /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors"), 
+        System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
         public DebugTextWriter()
             : base(new DebugOutStream(), Encoding.Unicode, 1024)
         {
