0afce25a5870ec46088afe4b7a9818a328de22b4
rowlandshaw_cp <rowland.shaw@gmail.com>
2013-04-22 20:58:43 +0000

    Fleshed out a touch (TFS Integration from 'tfsprevies' Id: 135;CreationDate: 2013-04-22 20:58:17 UTC;)

---
+++ Main/Source/Code/LocalAngle/ContactNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LocalAngle
+{
+    public class ContactNumber : BindableBase
+    {
+        private string _number;
+        public string Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                OnPropertyChanged("Number", ref _number, value);
+            }
+        }
+
+        private ContactType _contactType;
+        public ContactType ContactType
+        {
+            get
+            {
+                return _contactType;
+            }
+            set
+            {
+                OnPropertyChanged("ContactType", ref _contactType, value);
+            }
+        }
+    }
+}
