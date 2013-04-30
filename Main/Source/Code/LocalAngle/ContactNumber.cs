27e432c42b69faef3138494934f2fc1e4023f2b5
rowlandshaw_cp <SND\rowlandshaw_cp>
2013-04-30 17:49:45 +0000

    Flesh out establishement a bit more (TFS Integration from 'tfspreview' Id: 140;CreationDate: 2013-04-30 12:56:18 UTC;)

--- Main/Source/Code/LocalAngle/ContactNumber.cs
+++ Main/Source/Code/LocalAngle/ContactNumber.cs
@@ -1,26 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace LocalAngle
 {
     public class ContactNumber : BindableBase
     {
-        private string _number;
-        public string Number
+        private string _detail;
+        /// <summary>
+        /// Gets or sets the actual contact detail.
+        /// </summary>
+        /// <value>
+        /// The number.
+        /// </value>
+        [DataMember]
+        public string Detail
         {
             get
             {
-                return _number;
+                return _detail;
             }
             set
             {
-                OnPropertyChanged("Number", ref _number, value);
+                OnPropertyChanged("Detail", ref _detail, value);
             }
         }
 
         private ContactType _contactType;
+        /// <summary>
+        /// Gets or sets the type of the contact.
+        /// </summary>
+        /// <value>
+        /// The type of the contact.
+        /// </value>
+        [DataMember]
         public ContactType ContactType
         {
             get
--- Main/Source/Code/LocalAngle/Eatndrink/Establishment.cs
+++ Main/Source/Code/LocalAngle/Eatndrink/Establishment.cs
@@ -31,6 +31,12 @@ namespace LocalAngle.Eatndrink
         #region Public Properties
 
         private ICollection<ContactNumber> _contacts;
+        /// <summary>
+        /// Gets the contact numbers.
+        /// </summary>
+        /// <value>
+        /// The contact numbers.
+        /// </value>
         public ICollection<ContactNumber> ContactNumbers
         {
             get
@@ -40,6 +46,116 @@ namespace LocalAngle.Eatndrink
             private set
             {
                 _contacts = value;
+            }
+        }
+
+        private string _deliveryPoint;
+        /// <summary>
+        /// Gets or sets the delivery point for the establishment.
+        /// </summary>
+        /// <value>
+        /// The email address.
+        /// </value>
+        [DataMember]
+        [Column(DbType = "NVARCHAR(100)", UpdateCheck = UpdateCheck.Never)]
+        public string DeliveryPoint
+        {
+            get
+            {
+                return _deliveryPoint;
+            }
+            set
+            {
+                OnPropertyChanged("DeliveryPoint", ref _deliveryPoint, value);
+            }
+        }
+
+        private string _description;
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
+        [DataMember(IsRequired = true)]
+        [Required]
+        [Column(DbType = "NTEXT", UpdateCheck = UpdateCheck.Never)]
+        [DisplayName("Description")]
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                OnPropertyChanged("Description", ref _description, (value == null ? string.Empty : value.Trim()));
+            }
+        }
+
+        private string _email;
+        /// <summary>
+        /// Gets or sets the email address for the establishment.
+        /// </summary>
+        /// <value>
+        /// The email address.
+        /// </value>
+        [DataMember]
+        [Column(DbType = "NVARCHAR(255)", UpdateCheck = UpdateCheck.Never)]
+        public string EmailAddress
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                OnPropertyChanged("EmailAddress", ref _email, value);
+            }
+        }
+
+        private DateTime? _foodSafetyInspectionDate;
+        /// <summary>
+        /// Gets or sets the date of the most recent food safety inspection.
+        /// </summary>
+        /// <value>
+        /// The date of the most recent food safety inspection.
+        /// </value>
+        [DataMember]
+        [Column(UpdateCheck = UpdateCheck.Never)]
+        public DateTime FoodSafetyInspectionDate
+        {
+            get
+            {
+                return FoodSafetyInspectionDate;
+            }
+            set
+            {
+                OnPropertyChanged("FoodSafetyInspectionDate", ref _foodSafetyInspectionDate, value);
+            }
+        }
+
+        private string _foodSafetyRatingKey;
+        /// <summary>
+        /// Gets or sets the food safety rating key.
+        /// </summary>
+        /// <remarks>
+        /// The value can be combined with the images from the Foods Standards Agency to derive the correct image to display.
+        /// </remarks>
+        /// <value>
+        /// The food safety rating key.
+        /// </value>
+        [DataMember]
+        [Column(DbType = "NVARCHAR(50)", UpdateCheck = UpdateCheck.Never)]
+        public string FoodSafetyRatingKey
+        {
+            get
+            {
+                return _foodSafetyRatingKey;
+            }
+            set
+            {
+                OnPropertyChanged("FoodSafetyRatingKey", ref _foodSafetyRatingKey, value);
             }
         }
 
@@ -210,6 +326,12 @@ namespace LocalAngle.Eatndrink
         }
 
         private Uri _website;
+        /// <summary>
+        /// Gets or sets the website.
+        /// </summary>
+        /// <value>
+        /// The website.
+        /// </value>
         public Uri Website
         {
             get
@@ -221,6 +343,270 @@ namespace LocalAngle.Eatndrink
                 OnPropertyChanged("Website", ref _website, value);
             }
         }
+
+        #endregion
+
+        #region Public Static Methods
+
+        /// <summary>
+        /// Begins an asynchronous search for eat'n'drink establishments near the specified location.
+        /// </summary>
+        /// <param name="location">The location to search near.</param>
+        /// <param name="range">The range in miles to search for events in.</param>
+        /// <param name="since">The date to bring back changes since.</param>
+        /// <param name="credentials">The credentials.</param>
+        /// <param name="callback">The callback to call when the results are ready to be read</param>
+        /// <returns></returns>
+        public static IAsyncResult BeginSearchNear(AsyncCallback callback, Postcode location, double range, DateTime since, IOAuthCredentials credentials)
+        {
+            if (location == null)
+            {
+                throw new ArgumentNullException("location", "You must specify a location to search near");
+            }
+
+            OAuthWebRequest req = new OAuthWebRequest(new Uri("http://api.angle.uk.com/oauth/1.0/establishments/nearby"), credentials);
+            req.RequestParameters.Add(new RequestParameter("location", location.ToString()));
+            req.RequestParameters.Add(new RequestParameter("range", range.ToString(CultureInfo.InvariantCulture)));
+            if (since != default(DateTime))
+            {
+                req.RequestParameters.Add(new RequestParameter("since", since.ToUnixTime().ToString(CultureInfo.InvariantCulture)));
+            }
+
+            return req.BeginGetResponse(callback, req);
+        }
+
+        /// <summary>
+        /// Begins an asynchronous searches for eat'n'drink establishments near the specified location.
+        /// </summary>
+        /// <param name="location">The location to search near.</param>
+        /// <param name="range">The range in miles to search for events in.</param>
+        /// <param name="since">The date to bring back changes since.</param>
+        /// <param name="credentials">The credentials.</param>
+        /// <param name="callback">The callback to call when the results are ready to be read</param>
+        /// <returns></returns>
+        public static IAsyncResult BeginSearchNear(AsyncCallback callback, IGeoLocation location, double range, DateTime since, IOAuthCredentials credentials)
+        {
+            if (location == null)
+            {
+                throw new ArgumentNullException("location", "You must specify a location to search near");
+            }
+
+            OAuthWebRequest req = new OAuthWebRequest(new Uri("http://api.angle.uk.com/oauth/1.0/establishments/nearby"), credentials);
+            req.RequestParameters.Add(new RequestParameter("latitude", location.Latitude.ToString(CultureInfo.InvariantCulture)));
+            req.RequestParameters.Add(new RequestParameter("longitude", location.Longitude.ToString(CultureInfo.InvariantCulture)));
+            req.RequestParameters.Add(new RequestParameter("range", range.ToString(CultureInfo.InvariantCulture)));
+            if (since != default(DateTime))
+            {
+                req.RequestParameters.Add(new RequestParameter("since", since.ToUnixTime().ToString(CultureInfo.InvariantCulture)));
+            }
+
+            return req.BeginGetResponse(callback, req);
+        }
+
+        /// <summary>
+        /// Ends an asynchronous search for eat'n'drink establishments near a location.
+        /// </summary>
+        /// <param name="asyncResult">The async result.</param>
+        /// <returns></returns>
+        public static IEnumerable<Establishment> EndSearchNear(IAsyncResult asyncResult)
+        {
+            if (asyncResult == null)
+            {
+                throw new ArgumentNullException("asyncResult");
+            }
+
+            OAuthWebRequest req = (OAuthWebRequest)asyncResult.AsyncState;
+            HttpWebResponse res;
+            try
+            {
+                res = req.EndGetResponse(asyncResult) as HttpWebResponse;
+            }
+            catch (WebException)
+            {
+                // Sometimes we're seeing a 404, even though the server is not reporting one.
+                return null;
+            }
+
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(IEnumerable<Establishment>));
+
+            try
+            {
+                IEnumerable<Establishment> retval = (IEnumerable<Establishment>)ser.ReadObject(res.GetResponseStream());
+                return retval;
+            }
+            catch (ArgumentNullException)
+            {
+                // Sometimes we're seeing an ArgumentNullException, even though ser and the stream returned are not null
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Searches for eat'n'drink establishments near the specified location.
+        /// </summary>
+        /// <param name="location">The location to search near.</param>
+        /// <param name="range">The range in miles to search for events in.</param>
+        /// <param name="credentials">The credentials.</param>
+        /// <returns></returns>
+        public static IEnumerable<Establishment> SearchNear(Postcode location, double range, IOAuthCredentials credentials)
+        {
+            return SearchNear(location, range, default(DateTime), credentials);
+        }
+
+        /// <summary>
+        /// Searches for eat'n'drink establishments near the specified location.
+        /// </summary>
+        /// <param name="location">The location to search near.</param>
+        /// <param name="range">The range in miles to search for events in.</param>
+        /// <param name="since">The date to bring back changes since.</param>
+        /// <param name="credentials">The credentials.</param>
+        /// <returns></returns>
+        /// <remarks>If the postcode is not recognised by the server, it will fall back to making a guess based on the client IP address</remarks>
+        public static IEnumerable<Establishment> SearchNear(Postcode location, double range, DateTime since, IOAuthCredentials credentials)
+        {
+            IAsyncResult res = BeginSearchNear(callback => { }, location, range, since, credentials);
+            res.AsyncWaitHandle.WaitOne();
+            return EndSearchNear(res);
+        }
+
+        /// <summary>
+        /// Searches for eat'n'drink establishments near the specified location.
+        /// </summary>
+        /// <param name="location">The location to search near.</param>
+        /// <param name="range">The range in miles to search for events in.</param>
+        /// <param name="credentials">The credentials.</param>
+        /// <returns></returns>
+        public static IEnumerable<Establishment> SearchNear(IGeoLocation location, double range, IOAuthCredentials credentials)
+        {
+            return SearchNear(location, range, default(DateTime), credentials);
+        }
+
+        /// <summary>
+        /// Searches for eat'n'drink establishments near the specified location.
+        /// </summary>
+        /// <param name="location">The location to search near.</param>
+        /// <param name="range">The range in miles to search for events in.</param>
+        /// <param name="since">The date to bring back changes since.</param>
+        /// <param name="credentials">The credentials.</param>
+        /// <returns></returns>
+        public static IEnumerable<Establishment> SearchNear(IGeoLocation location, double range, DateTime since, IOAuthCredentials credentials)
+        {
+            IAsyncResult res = BeginSearchNear(callback => { }, location, range, since, credentials);
+#if SILVERLIGHT
+            while (!res.IsCompleted)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+#else
+            res.AsyncWaitHandle.WaitOne();
+#endif
+            return EndSearchNear(res);
+        }
+
+#if NETFX_CORE
+
+        /// <summary>
+        /// Searches for eat'n'drink establishments near the specified location.
+        /// </summary>
+        /// <param name="location">The location to search near.</param>
+        /// <param name="range">The range in miles to search for events in.</param>
+        /// <param name="since">The date to bring back changes since.</param>
+        /// <param name="credentials">The credentials.</param>
+        /// <param name="callback">The callback to call when the results are ready to be read</param>
+        /// <returns></returns>
+        public async static Task<IEnumerable<Establishment>> SearchNearAsync(Postcode location, double range, DateTime since, IOAuthCredentials credentials)
+        {
+            if (location == null)
+            {
+                throw new ArgumentNullException("location", "You must specify a location to search near");
+            }
+
+            OAuthWebRequest req = new OAuthWebRequest(new Uri("http://api.angle.uk.com/oauth/1.0/eatndrink/nearby"), credentials);
+            req.RequestParameters.Add(new RequestParameter("location", location.ToString()));
+            req.RequestParameters.Add(new RequestParameter("range", range.ToString(CultureInfo.InvariantCulture)));
+            if (since != default(DateTime))
+            {
+                req.RequestParameters.Add(new RequestParameter("since", since.ToUnixTime().ToString(CultureInfo.InvariantCulture)));
+            }
+
+            HttpWebResponse res;
+            try
+            {
+                res = await req.GetResponseAsync() as HttpWebResponse;
+            }
+            catch (WebException)
+            {
+                // Sometimes we're seeing a 404, even though the server is not reporting one.
+                return null;
+            }
+
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(IEnumerable<Establishment>));
+
+            try
+            {
+                IEnumerable<Establishment> retval = (IEnumerable<Establishment>)ser.ReadObject(res.GetResponseStream());
+                return retval;
+            }
+            catch (ArgumentNullException)
+            {
+                // Sometimes we're seeing an ArgumentNullException, even though ser and the stream returned are not null
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Searches for eat'n'drink establishments near the specified location.
+        /// </summary>
+        /// <param name="location">The location to search near.</param>
+        /// <param name="range">The range in miles to search for events in.</param>
+        /// <param name="since">The date to bring back changes since.</param>
+        /// <param name="credentials">The credentials.</param>
+        /// <returns></returns>
+        public async static Task<IEnumerable<Establishment>> SearchNearAsync(IGeoLocation location, double range, DateTime since, IOAuthCredentials credentials)
+        {
+            if (location == null)
+            {
+                throw new ArgumentNullException("location", "You must specify a location to search near");
+            }
+
+            OAuthWebRequest req = new OAuthWebRequest(new Uri("http://api.angle.uk.com/oauth/1.0/eatndrink/nearby"), credentials);
+            req.RequestParameters.Add(new RequestParameter("latitude", location.Latitude.ToString(CultureInfo.InvariantCulture)));
+            req.RequestParameters.Add(new RequestParameter("longitude", location.Longitude.ToString(CultureInfo.InvariantCulture)));
+            req.RequestParameters.Add(new RequestParameter("range", range.ToString(CultureInfo.InvariantCulture)));
+            if (since != default(DateTime))
+            {
+                req.RequestParameters.Add(new RequestParameter("since", since.ToUnixTime().ToString(CultureInfo.InvariantCulture)));
+            }
+
+            HttpWebResponse res;
+            try
+            {
+                res = await req.GetResponseAsync() as HttpWebResponse;
+            }
+            catch (WebException)
+            {
+                // Sometimes we're seeing a 404, even though the server is not reporting one.
+                return null;
+            }
+
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(IEnumerable<Establishment>));
+
+            try
+            {
+                IEnumerable<Establishment> retval = (IEnumerable<Establishment>)ser.ReadObject(res.GetResponseStream());
+                return retval;
+            }
+            catch (ArgumentNullException)
+            {
+                // Sometimes we're seeing an ArgumentNullException, even though ser and the stream returned are not null
+            }
+
+            return null;
+        }
+
+#endif
 
         #endregion
     }
